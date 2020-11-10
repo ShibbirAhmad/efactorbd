eb982a02218bb96f5cd1d087be45bd9acd1afea2
fahim27 <fahimmbpi@gmail.com>
2020-11-10 15:52:07 +0600

    frontend blading completed

---
+++ resources/views/Frontend/partials/css.blade.php
@@ -0,0 +1,13 @@
+
+@stack('extra-css')
+
+<!-- CSS Part Start-->
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/js/bootstrap/css/bootstrap.min.css ') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/font-awesome/css/font-awesome.min.css ') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/stylesheet.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/owl.carousel.css ') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/owl.transitions.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/responsive.css ') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/stylesheet-skin3.css') }}" />
+<link href='//fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+<!-- CSS Part End-->
---
+++ resources/views/Frontend/partials/js.blade.php
@@ -0,0 +1,13 @@
+
+
+
+@stack('extra-js') 
+
+<!-- JS Part Start-->
+<script type="text/javascript" src="{{ asset('frontend/js/jquery-2.1.1.min.js ') }}"></script>
+<script type="text/javascript" src="{{ asset('frontend/js/bootstrap/js/bootstrap.min.js ') }}"></script>
+<script type="text/javascript" src="{{ asset('frontend/js/jquery.easing-1.3.min.js ') }}"></script>
+<script type="text/javascript" src="{{ asset('frontend/js/jquery.dcjqaccordion.min.js ') }}"></script>
+<script type="text/javascript" src="{{ asset('frontend/js/owl.carousel.min.js ') }}"></script>
+<script type="text/javascript" src="{{ asset('frontend/js/custom.js ') }}"></script>
+<!-- JS Part End-->
